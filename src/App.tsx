e2dbc5c3acaebedd1d72a7ddf4a486c6189ab3d4
jakub ryba <kuba.2001@email.cz>
2024-03-03 09:59:08 +0100

    pipik

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import ResponsiveAppBar from './Header'
+
+import BasicTabs from './Tab'
+
+function App() {
+
+
+  return (
+    <>
+    <ResponsiveAppBar/>
+    <BasicTabs/>
+    </>
+  )
+}
+
+export default App
